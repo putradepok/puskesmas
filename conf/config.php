2be72d72388d8ab499ae0dfed43b27f36b387437
unknown <baguspurnomoputrasetiawan146@gmail.com>
2025-01-23 08:30:05 +0700

    project puskesmas

---
+++ conf/config.php
@@ -0,0 +1,10 @@
+<?php
+$koneksi = mysqli_connect('localhost','root','','db_sikampus');
+// Mengecek KOneksi 
+// if(!$koneksi){
+//     die ("koneksi gagal:". mysqli_connect_eror());
+// }
+// else{
+//     echo "koneksi berhasil";
+// }
+?>
